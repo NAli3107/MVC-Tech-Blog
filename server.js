00e8388f4dca824ccff17982268643b3a0a6616f
Nadira Ali <nadiraali188a@gmail.com>
2022-03-31 21:09:44 +0100

    created server.js file and requiring express, session and handlebars

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
