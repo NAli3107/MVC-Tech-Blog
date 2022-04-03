b9bff1ff4115f288bb4ef9a8850c9b92a8d6633a
Nadira Ali <nadiraali188a@gmail.com>
2022-04-03 15:48:25 +0100

    associations and connections created between the three models

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require("./user");
+const BlogPost = require("./blogPost");
+const Comment = require("./comment");
+
+User.hasMany(BlogPost, {
+  foreignKey: "user_id",
+});
+
+BlogPost.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+BlogPost.hasMany(Comment, {
+  foreignKey: "blogPost_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: "blogPost_id",
+  onDelete: "cascade",
+});
+
+module.exports = { User, BlogPost, Comment};
