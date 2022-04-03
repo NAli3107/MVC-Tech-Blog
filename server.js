f5871d06dd05911cbcca094f290a855e67942add
Nadira Ali <nadiraali188a@gmail.com>
2022-04-03 14:34:51 +0100

    app now listening on port 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const path = require('path');
 const helpers = require('./utils/helpers.js');
 
@@ -14,3 +14,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening ${PORT}`));
+});
