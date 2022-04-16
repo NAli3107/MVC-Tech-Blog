f345a933909dd2d4b2096e451e48b3022b6807b3
Nadira Ali <nadiraali188a@gmail.com>
2022-04-16 19:13:52 +0100

    post, put and delete requests for postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,9 +1,61 @@
-/**
- post route to create new post
+const router = require('express').Router();
+const { BlogPost } = require('../../models');
+const withAuth = require('../../utils/auth');
 
- put route to update an existing post
+/* post route to create new blog post */
 
- delete route to delete
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlogPost = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
- mini project examples
- */
+    res.status(200).json(newBlogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+/* delete route to delete blog post */
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No blog post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* put route to update an existing blog post */
+
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+      const postData = await BlogPost.update(req.body, {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+      if (!postData) {
+        res.status(404).json({ message: "No blog post found with this id!" });
+        return;
+      }
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
