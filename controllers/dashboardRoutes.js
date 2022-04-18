c6e27dce16329982feb551a1ebd06f7b7b2a1e87
Nadira Ali <nadiraali188a@gmail.com>
2022-04-18 12:04:29 +0100

    dashboard end point amended - now in homeRoutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -7,27 +7,27 @@
 
  */
 
- const router = require('express').Router();
- const { User } = require('../models');
- const withAuth = require('../utils/auth')
+//  const router = require('express').Router();
+//  const { User } = require('../models');
+//  const withAuth = require('../utils/auth')
 
- router.get('/dashboard', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
+//  router.get('/dashboard', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Project }],
+//       });
   
-      const user = userData.get({ plain: true });
+//       const user = userData.get({ plain: true });
   
-      res.render('dashboard', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//       res.render('dashboard', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
   module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, BlogPost } = require("../models");
-// const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -60,6 +60,25 @@ router.get("/posts/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/dashboard', withAuth, async (req, res) => {
+   try {
+     // Find the logged in user based on the session ID
+     const userData = await User.findByPk(req.session.user_id, {
+       attributes: { exclude: ['password'] },
+       include: [{ model: Project }],
+     });
+ 
+     const user = userData.get({ plain: true });
+ 
+     res.render('dashboard', {
+       ...user,
+       logged_in: true
+     });
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ });
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
