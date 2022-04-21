8e418d0a7272f6146b7f74aa68c32c1e29bf93bb
Nadira Ali <nadiraali188a@gmail.com>
2022-04-21 18:12:57 +0100

    update

--- models/Post.js
+++ models/Post.js
@@ -18,18 +18,18 @@ BlogPost.init(
     content: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
+    // },
+    // date_created: {
+    //   type: DataTypes.DATE,
+    //   allowNull: false,
+    //   defaultValue: DataTypes.NOW,
+    // },
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "user",
+    //     key: "id",
+    //   },
     },
   },
   {
